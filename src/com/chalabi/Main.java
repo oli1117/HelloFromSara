cf77070453d67a92d381438a1cf79392cb1b4a19
Sara Chalabi <chalabi85@gmail.com>
2021-10-05 19:29:56 +0300

    Initial commit

---
+++ src/com/chalabi/Main.java
@@ -0,0 +1,9 @@
+package com.chalabi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, My name is Sara");
+    }
+}
